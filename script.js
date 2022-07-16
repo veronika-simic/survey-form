f84de3a84db8a8273db2240c72dfa41322eb5f6c
veronika-simic <veronikasimic21@gmail.com>
2022-07-16 16:45:39 +0200

    Fix issue for fourth page

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 const setStep = step => {
     document.querySelectorAll('.step-content').forEach(element => element.style.display = 'none'); /* first hide all pages except the one with class current*/
     document.querySelector("[data-step='" + step + "']").style.display = 'block'; /* display the selected page */
+   /* change dots in header */
+   document.querySelectorAll('.steps .step').forEach((element, index) => {
+   index < step-1 ? element.classList.add("complete") : element.classList.remove("complete"); /* complete or not */
+   index == step-1 ? element.classList.add("current") : element.classList.remove("current"); /* current or not */
    
+})
 
 }
 document.querySelectorAll("[data-set-step]").forEach(element => { /* select all buttons */
